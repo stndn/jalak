c35928b90fdad9390c11b06a125805f0ce267f38
Sutan <stndn@users.noreply.github.com>
2023-10-07 00:33:02 +0800

    Added the weather DAG

---
+++ dags/weather_data.py
@@ -0,0 +1,49 @@
+import pendulum
+
+from airflow.decorators import dag, task
+from airflow.providers.ssh.operators.ssh import SSHOperator
+
+default_args = {
+  "owner": "sutan",
+  "owner_links": { 'sutan': 'https://flow.71182141.xyz/' },
+  "provide_context": True,
+  "retries": 0,
+}
+
+@dag(
+  dag_id='get-and-save-weather-data',
+  description='Download weather data via API calls and save to S3 storage',
+  default_args=default_args,
+  schedule='@hourly',
+  catchup=False,
+  start_date=pendulum.datetime(2023, 10, 1),
+  tags=['merpati'],
+)
+
+def GetWeatherData():
+  """
+  ### Download and store weather information from Open-Meteo
+  Executes Python scripts hosted in specified `ssh_conn_id` to
+  [download weather information](https://github.com/stndn/merpati/tree/main/weather)
+  and stores them in S3
+  """
+
+  get_weather = SSHOperator(
+      task_id='get-weather',
+      ssh_conn_id='rumah_merpati',
+      command='cd /home/garuda/app/merpati/weather && make get-weather'
+    )
+
+  save_weather = SSHOperator(
+      task_id='save-weather',
+      ssh_conn_id='rumah_merpati',
+      command='cd /home/garuda/app/merpati/weather && make save-weather'
+    )
+
+  get_weather >> log_date
+
+
+_ = GetWeatherData()
+
+if __name__ == "__main__":
+    dag.test()
